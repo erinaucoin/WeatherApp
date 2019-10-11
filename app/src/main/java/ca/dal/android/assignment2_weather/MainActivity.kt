514d1a3080a92a5f978f8e3cf31735b5efb6742e
Erin A <erin_@DESKTOP-R1LPDHL>
2019-10-11 00:15:01 -0300

    data is being properly parsed, added sharedpreferences, locked screen to portrait orientation

--- app/src/main/java/ca/dal/android/assignment2_weather/MainActivity.kt
+++ app/src/main/java/ca/dal/android/assignment2_weather/MainActivity.kt
@@ -1,50 +1,55 @@
 package ca.dal.android.assignment2_weather
 
 import android.content.Context
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.view.inputmethod.InputMethodManager
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
-import android.widget.Toast
+import android.widget.*
 import com.android.volley.Request
 import com.android.volley.Response
 import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
+import kotlinx.android.synthetic.main.activity_main.view.*
 import org.json.JSONObject
+//TODO readme file, shared preferences, block horizontal view
+//TODO take care of layout warnings
+//TODO easier city input
 
-val WEATHER_URL = "https://api.openweathermap.org/data/2.5/weather?q="
-val API_KEY_URL = "&appid=e6b678d376b569e9637c39ce5dc40371"
+const val WEATHER_URL = "https://api.openweathermap.org/data/2.5/weather?q="
+const val API_KEY_URL = "&appid=e6b678d376b569e9637c39ce5dc40371"
+const val PREV_CITY = "previous_city"
+const val SHAREDPREF_NAME = "weatherAppPref"
 
 //https://api.openweathermap.org/data/2.5/weather?q=halifax&appid=e6b678d376b569e9637c39ce5dc40371
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var sharedPreferences: SharedPreferences
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        sharedPreferences = getSharedPreferences(SHAREDPREF_NAME, Context.MODE_PRIVATE)
+
         setContentView(R.layout.activity_main)
         val searchButton: Button = findViewById(R.id.search_button)
         searchButton.setOnClickListener{
             val city = findViewById<EditText>(R.id.city_text_field)
-            //Toast.makeText(this, city.text, Toast.LENGTH_SHORT).show()
+            //TODO clean this city text somehow
 
             // Hide the keyboard.
             val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
             imm.hideSoftInputFromWindow(it.windowToken, 0)
-
-            //val weatherInfo = WeatherInfo("halifax", "clear", "overcast", 274.15, 274.15, 274.15, 86, 90)
 
             getWeatherInfo(WEATHER_URL + city.text + API_KEY_URL)
         }
     }
 
     private fun getWeatherInfo(url: String){
-        //Toast.makeText(this, "getInfo", Toast.LENGTH_LONG).show()
         Volley.newRequestQueue(this).add(
             StringRequest(Request.Method.GET, url,
                 Response.Listener<String> { response ->
-                    //Toast.makeText(this, "no error", Toast.LENGTH_LONG).show()
                     val weatherInfo = extractWeatherInfo(JSONObject(response))
                     assignWeatherInfo(weatherInfo)
                 },
@@ -58,15 +63,13 @@ class MainActivity : AppCompatActivity() {
         return with(response) {
             WeatherInfo(
                 getString("name"),
-                getString("main"),
-                getString("description"),
-                275.15, 280.15, 270.15, 75, 90
-//                getString("temp").toDouble(),
-//                getString("temp_max").toDouble(),
-//                getString("temp_min").toDouble(),
-//                getString("humidity").toInt(),
-//                getString("all").toInt()
-            //TODO I don't think this will work with the arrays
+                getJSONArray("weather").getJSONObject(0).getString("main"),
+                getJSONArray("weather").getJSONObject(0).getString("description"),
+                getJSONObject("main").getString("temp").toDouble(),
+                getJSONObject("main").getString("temp_max").toDouble(),
+                getJSONObject("main").getString("temp_min").toDouble(),
+                getJSONObject("main").getString("humidity").toInt(),
+                getJSONObject("clouds").getString("all").toInt()
             )
         }
     }
@@ -76,10 +79,25 @@ class MainActivity : AppCompatActivity() {
         findViewById<TextView>(R.id.main_weather).text = weatherInfo.main
         findViewById<TextView>(R.id.description).text = weatherInfo.description
         findViewById<TextView>(R.id.temp).text = (weatherInfo.temperature - 273.15).toInt().toString() + "°C"
-        findViewById<TextView>(R.id.max_temp).text = "Max Temp:  " + (weatherInfo.maxtemp - 273.15).toString() + "°C"
-        findViewById<TextView>(R.id.min_temp).text = "Min Temp:  " + (weatherInfo.mintemp - 273.15).toString() + "°C"
+        findViewById<TextView>(R.id.max_temp).text = "Max Temp:  " + (weatherInfo.maxtemp - 273.15).toInt().toString() + "°C"
+        findViewById<TextView>(R.id.min_temp).text = "Min Temp:  " + (weatherInfo.mintemp - 273.15).toInt().toString() + "°C"
         findViewById<TextView>(R.id.humidity_pc).text = weatherInfo.humidity.toString() + "%"
         findViewById<TextView>(R.id.cloud_pc).text = weatherInfo.clouds.toString() + "%"
+        findViewById<FrameLayout>(R.id.weather_frame).visibility = View.VISIBLE
+    }
+
+    override fun onStop() {
+        super.onStop()
+        with(sharedPreferences.edit()){
+            putString(PREV_CITY, findViewById<TextView>(R.id.city_name).text.toString())
+            apply()
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        if(sharedPreferences.getString(PREV_CITY, "") != "")
+        getWeatherInfo(WEATHER_URL + sharedPreferences.getString(PREV_CITY, "") + API_KEY_URL)
     }
 }
 
